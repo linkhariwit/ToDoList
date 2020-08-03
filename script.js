fbb251617249b4c5c550bb02a12ed5b6d212bacf
linkhariwit <links.hariwit@gmail.com>
2020-08-03 18:52:56 +0700

    8_3_2020

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // Create a "close" button and append it to each list item
+var save=[]
+if(localStorage.getItem('myCat')!=""){
+  var cat = localStorage.getItem('myCat').split(",");
+  for(i=0;i<cat.length;i++){
+    var li = document.createElement("li");
+  
+    var inputValue = cat[i];
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    save.push(inputValue)
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("delBtn").style.display="inline";
+}
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -12,6 +26,7 @@ for (i = 0; i < myNodelist.length; i++) {
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
+
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
@@ -45,7 +60,9 @@ function newElement() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
+  save.push(inputValue);
+  localStorage.setItem('myCat', save);
+  document.getElementById("delBtn").style.display="inline";
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
@@ -53,6 +70,15 @@ function newElement() {
     }
   }
 }
+//delallbtn
+function del(){
+  localStorage.removeItem('myCat');
+  location.reload();
+ 
+
+
+}
+//enterbtn
 function handle(e){
     if(e.keyCode === 13){
         e.preventDefault(); 
@@ -60,62 +86,6 @@ function handle(e){
 
     }
 }
-/*var images = [
-    "https://i.pinimg.com/originals/92/46/65/92466543bfebf878eabbf9818b24927e.jpg",
-    "https://mcdn.wallpapersafari.com/medium/92/32/0onWFG.jpg",
-    "https://i.pinimg.com/originals/60/48/12/60481218c564666e54f914bf95332897.jpg"
-  ]
-var imageHead = document.getElementById("image-head");
 
-setInterval(function() {
-    var j = 0;
-      imageHead.style.backgroundImage = "url(" + images[i] + ")";
-      j = j + 1;
-      if (j == images.length) {
-        j =  0;
-      }
-}, 5000);*/
-// Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
-dragElement(document.getElementById("container"));
+  
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
